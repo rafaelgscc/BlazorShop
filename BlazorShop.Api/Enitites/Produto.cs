dede4a064165377e2d8fbf45d1a94e5b6ca1d1d8
rafaelgscc <rafaelgscc@gmail.com>
2025-04-11 10:01:16 -0300

    Adicionando migrations

--- BlazorShop.Api/Enitites/Produto.cs
+++ BlazorShop.Api/Enitites/Produto.cs
@@ -1,13 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorShop.Api.Enitites;
 
 public class Produto
 {
      public int Id { get; set; }
+     
+     [MaxLength(100)]
      public string Nome { get; set; } = string.Empty;
+     
+     [MaxLength(200)]
      public string Descricao { get; set; } = string.Empty;
+
+     [MaxLength(200)]
      public string ImagemUrl { get; set; } = string.Empty;
+
+     [Column(TypeName ="decimal(10,2)")]
      public decimal Preco {get; set; }
      public int Quantidade {get; set; }
 
--- BlazorShop.Api/Enitites/Usuario.cs
+++ BlazorShop.Api/Enitites/Usuario.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorShop.Api.Enitites;
 
 public class Usuario
 {
     public int Id {get; set;}
+
+    [MaxLength(100)]
     public string NomeUsuario {get; set;} = string.Empty;
 
     public Carrinho? Carrinho {get; set;}
