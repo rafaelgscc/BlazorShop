648051160e86b0a4b1e2adffcd7927e52673fcca
rafaelgscc <rafaelgscc@gmail.com>
2025-04-11 09:42:11 -0300

    Adicionando itens do banco de dados

--- BlazorShop.Api/Enitites/Carrinho.cs
+++ BlazorShop.Api/Enitites/Carrinho.cs
@@ -5,7 +5,9 @@ namespace BlazorShop.Api.Enitites;
 public class Carrinho
 {
     public int Id { get; set; }
-    public int UsuarioId { get; set; }
+
+    //workaround
+    public string UsuarioId { get; set; } = string.Empty;
 
     public ICollection<CarrinhoItem> Itens {get; set;} = new List<CarrinhoItem>();
 }
--- BlazorShop.Api/Enitites/Usuario.cs
+++ BlazorShop.Api/Enitites/Usuario.cs
@@ -6,4 +6,6 @@ public class Usuario
 {
     public int Id {get; set;}
     public string NomeUsuario {get; set;} = string.Empty;
+
+    public Carrinho? Carrinho {get; set;}
 }
