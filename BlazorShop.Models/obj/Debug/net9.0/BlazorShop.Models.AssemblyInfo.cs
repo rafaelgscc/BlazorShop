4d07d62805252dc812652194003506ff4e28adb0
rafaelgscc <rafaelgscc@gmail.com>
2025-04-14 12:29:50 -0300

    Adicionando itens de frontend e pasta services

--- BlazorShop.Models/obj/Debug/net9.0/BlazorShop.Models.AssemblyInfo.cs
+++ BlazorShop.Models/obj/Debug/net9.0/BlazorShop.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorShop.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113b7e6fe93deb0088a21c3f1926b23ffdaaf00b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cfe14f316f7584a461370f5be888fa0ea6e8710")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorShop.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorShop.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
